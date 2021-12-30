8673efe3282eb039527732ee51620c0b2c2e4fe7
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-12-31 01:11:48 +0200

    Operand 2 Menu

--- CommMenu.asm
+++ CommMenu.asm
@@ -85,6 +85,11 @@
     ClearSpace db '     ', '$'
     num db 30,?,30 DUP(?)       
     StrSize db ?
+    num2 db 30,?,30 DUP(?)       
+    StrSize2 db ?
+    a EQU 1000
+    B EQU 100
+    C EQU 10
 
     ; Variables Memory Locations and data
     CommStringSize EQU  6
@@ -101,12 +106,20 @@
 
 
     selectedComm db -1, '$'
+
     selectedOp1Type db -1, '$'  
-    selectedReg  db -1, '$'
-    selectedAddReg db -1, '$'
-    selectedMem  db -1, '$'
+    selectedOp1Reg  db -1, '$'
+    selectedOp1AddReg db -1, '$'
+    selectedOp1Mem  db -1, '$'
     Op1Val dw 0
     Op1Valid db 1               ; 0 if Invalid 
+
+    selectedOp2Type db -1, '$'
+    selectedOp2Reg  db -1, '$'
+    selectedOp2AddReg db -1, '$'
+    selectedOp2Mem  db -1, '$'
+    Op2Val dw 0
+    Op2Valid db 1               ; 0 if Invalid 
 
     
 
@@ -120,11 +133,10 @@
     ; Cursor Locations
     MenmonicCursorLoc EQU 0000H
     Op1CursorLoc EQU 0006H
+    CommaCursorLoc EQU 000BH
     Op2CursorLoc EQU 000CH
     
-    a EQU 1000
-    B EQU 100
-    C EQU 10
+    
  
     
 
@@ -138,7 +150,13 @@
         
         CALL MnemonicMenu
         CALL Op1Menu
+        MOV DX, CommaCursorLoc
+        CALL SetCursor
+        mov dl, ','
+        CALL DisplayChar
+        CALL Op2Menu
 
+        ; Test Messages 
         mov dx, offset mesCom
         CALL DisplayString
         mov dl, selectedComm
@@ -153,22 +171,34 @@
 
         mov dx, offset mesReg
         CALL DisplayString
-        mov dl, selectedReg
+        mov dl, selectedOp1Reg
         add dl, '0'
         CALL DisplayChar
 
         mov dx, offset mesMem
         CALL DisplayString
-        mov dl, selectedMem
+        mov dl, selectedOp1Mem
         add dl, '0'
         CALL DisplayChar
 
-        mov dx, offset mesVal
+        
+
+
+        mov dx, offset mesOp1Type
         CALL DisplayString
-        mov dl, byte ptr Op1Val
+        mov dl, selectedOp2Type
         add dl, '0'
         CALL DisplayChar
-        mov dl, byte ptr Op1Val+1
+
+        mov dx, offset mesReg
+        CALL DisplayString
+        mov dl, selectedOp2Reg
+        add dl, '0'
+        CALL DisplayChar
+
+        mov dx, offset mesMem
+        CALL DisplayString
+        mov dl, selectedOp2Mem
         add dl, '0'
         CALL DisplayChar
 
@@ -355,6 +385,75 @@
             
         ret
     Op1TypeMenu ENDP
+    Op2TypeMenu PROC
+
+        
+        mov ah, 9
+        mov dx, offset Reg
+        int 21h
+
+        CheckKey_Op2Type:
+            ; Clear buffer
+            mov ah,07
+            int 21h
+            CALL WaitKeyPress
+
+        Push ax
+        PUSH dx 
+            ; Clear buffer
+            mov ah,07
+            int 21h
+            ; Reset Cursor
+            mov ah,2
+            mov dx, Op2CursorLoc
+            int 10h
+        pop dx 
+        pop ax
+
+        ; Check if pressed is Up or down or Enter
+        cmp ah, UpArrowScanCode                          
+        jz CommUp_Op2Type
+        cmp ah, DownArrowScanCode
+        jz CommDown_Op2Type
+        cmp ah, EnterScanCode
+        jz Selected_Op2Type
+        JMP CheckKey_Op2Type
+
+
+        CommUp_Op2Type:
+            mov ah, 9
+            ; Check overflow
+                cmp dx, offset Reg
+                jnz NotOverflow_Op2Type
+                mov dx, offset Value           ; OpTypeLastChoiceLoc
+                add dx, CommStringSize
+            NotOverflow_Op2Type:
+                sub dx, CommStringSize
+                int 21h
+                jmp CheckKey_Op2Type
+        
+        CommDown_Op2Type:
+            mov ah, 9
+            ; Check End of file
+                cmp dx, offset Value           ; OpTypeLastChoiceLoc
+                jnz NotEOF_Op2Type
+                mov dx, offset Reg
+                sub dx, CommStringSize
+            NotEOF_Op2Type:
+                add dx, CommStringSize
+                int 21h
+                jmp CheckKey_Op2Type
+        
+        Selected_Op2Type:
+            ; Detecting index of selected command
+            mov ax, dx
+            sub ax, offset Reg         ; OpTypeFirstChoiceLoc
+            mov bl, CommStringSize
+            div bl                                      ; Op=byte: AL:=AX / Op 
+            mov selectedOp2Type, al
+            
+        ret
+    Op2TypeMenu ENDP
     Op1Menu PROC
 
         ; Set Cursor
@@ -444,7 +543,7 @@
                 SUB AX, offset RegAX              ; RegFirstChoiceLocation
                 mov bl, CommStringSize
                 div bl                                      ; Op=byte: AL:=AX / Op 
-                mov selectedReg, al
+                mov selectedOp1Reg, al
                 ; NEEDS TO ADD A RETURN OR ENDING JUMP HERE
                 JMP RETURN
 
@@ -511,7 +610,7 @@
                 SUB AX, offset AddRegAX              ; RegFirstChoiceLocation
                 mov bl, CommStringSize
                 div bl                                      ; Op=byte: AL:=AX / Op 
-                mov selectedAddReg, al
+                mov selectedOp1AddReg, al
                 ; NEEDS TO ADD A RETURN OR ENDING JUMP HERE
                 JMP RETURN
         
@@ -579,7 +678,7 @@
                 SUB AX, offset Mem0
                 mov bl, CommStringSize
                 div bl                                      ; Op=byte: AL:=AX / Op 
-                mov selectedMem, al
+                mov selectedOp1Mem, al
 
                 JMP RETURN
 
@@ -702,4 +801,351 @@
 
             RET
     Op1Menu ENDP
+    Op2Menu PROC
+
+        ; Set Cursor
+        mov ah,2
+        mov dx, Op2CursorLoc 
+        int 10h
+
+        CALL Op2TypeMenu
+
+        ; Set Cursor
+        mov ah,2
+        mov dx, Op2CursorLoc 
+        int 10h
+
+
+        ; NEEDS TO CHANGE 
+        CMP selectedOp2Type, RegIndex     
+        JZ ChooseReg_Op2Menu
+        CMP selectedOp2Type, AddRegIndex
+        JZ ChooseAddReg_Op2Menu
+        CMP selectedOp2Type, MemIndex
+        JZ ChooseMem_Op2Menu
+        CMP selectedOp2Type, ValIndex
+        JZ EnterVal_Op2Menu
+        jmp InvalidOp2Type
+
+        ChooseReg_Op2Menu: 
+
+            ; Display Command
+            mov ah, 9
+            mov dx, offset RegAX
+            int 21h
+
+            CheckKey_RegType_Op2Menu:
+                CALL WaitKeyPress
+
+
+            Push ax
+            PUSH dx 
+                ; Clear buffer
+                mov ah,07
+                int 21h
+                ; Reset Cursor
+                mov ah,2
+                mov dx, Op2CursorLoc
+                int 10h
+            pop dx 
+            pop ax
+
+            ; Check if pressed is Up or down or Enter
+            cmp ah, UpArrowScanCode                          
+            jz CommUp_RegType_Op2Menu
+            cmp ah, DownArrowScanCode
+            jz CommDown_RegType_Op2Menu
+            cmp ah, EnterScanCode
+            jz Selected2
+            jmp CheckKey_RegType_Op2Menu
+
+
+            CommUp_RegType_Op2Menu:
+                mov ah, 9
+                ; Check overflow
+                    cmp dx, offset RegAX              ; RegFirstChoiceLocation ; the start of combobox
+                    jnz NotOverflow_RegType_Op2Menu
+                    mov dx, offset RegDI              ; RegLastChoiceLocation ; last one to overcome overflow
+                    add dx, CommStringSize
+                NotOverflow_RegType_Op2Menu:
+                    sub dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_RegType_Op2Menu
+            
+            CommDown_RegType_Op2Menu:
+                mov ah, 9
+                ; Check End of file
+                    cmp dx, offset RegDI              ; RegLastChoiceLocation
+                    jnz NotEOF_RegType_Op2Menu
+                    mov dx, offset RegAX              ; RegFirstChoiceLocation
+                    sub dx, CommStringSize
+                NotEOF_RegType_Op2Menu:
+                    add dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_RegType_Op2Menu
+            
+            Selected_RegType_Op2Menu:
+                ; Detecting index of selected command
+                mov ax, dx
+                SUB AX, offset RegAX              ; RegFirstChoiceLocation
+                mov bl, CommStringSize
+                div bl                                      ; Op=byte: AL:=AX / Op 
+                mov selectedOp2Reg, al
+                ; NEEDS TO ADD A RETURN OR ENDING JUMP HERE
+                JMP RETURN_Op2Menu
+
+        ChooseAddReg_Op2Menu:
+
+            ; Display Command
+            mov ah, 9
+            mov dx, offset AddRegAX
+            int 21h
+
+            CheckKey_AddReg_Op2Menu:
+                CALL WaitKeyPress
+
+
+            Push ax
+            PUSH dx 
+                ; Clear buffer
+                mov ah,07
+                int 21h
+                ; Reset Cursor
+                mov ah,2
+                mov dx, Op2CursorLoc
+                int 10h
+            pop dx 
+            pop ax
+
+            ; Check if pressed is Up or down or Enter
+            cmp ah, UpArrowScanCode                          
+            jz CommUp_AddReg_Op2Menu
+            cmp ah, DownArrowScanCode
+            jz CommDown_AddReg_Op2Menu
+            cmp ah, EnterScanCode
+            jz Selected_AddReg
+            jmp CheckKey_AddReg_Op2Menu
+
+
+            CommUp_AddReg_Op2Menu:
+                mov ah, 9
+                ; Check overflow
+                    cmp dx, offset AddRegAX              ; RegFirstChoiceLocation ; the start of combobox
+                    jnz NotOverflow_AddReg_Op2Menu
+                    mov dx, offset AddRegDI              ; RegLastChoiceLocation ; last one to overcome overflow
+                    add dx, CommStringSize
+                NotOverflow_AddReg_Op2Menu:
+                    sub dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_AddReg_Op2Menu
+            
+            CommDown_AddReg_Op2Menu:
+                mov ah, 9
+                ; Check End of file
+                    cmp dx, offset AddRegDI              ; RegLastChoiceLocation
+                    jnz NotEOF_AddReg_Op2Menu
+                    mov dx, offset AddRegAX              ; RegFirstChoiceLocation
+                    sub dx, CommStringSize
+                NotEOF_AddReg_Op2Menu:
+                    add dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_AddReg_Op2Menu
+            
+            Selected_AddReg_Op2Menu:
+                ; Detecting index of selected command
+                mov ax, dx
+                SUB AX, offset AddRegAX              ; RegFirstChoiceLocation
+                mov bl, CommStringSize
+                div bl                                      ; Op=byte: AL:=AX / Op 
+                mov selectedOp2AddReg, al
+                ; NEEDS TO ADD A RETURN OR ENDING JUMP HERE
+                JMP RETURN_Op2Menu
+        
+        ChooseMem_Op2Menu:
+       
+            ; Display memory
+            
+            mov ah, 9
+            mov dx, offset Mem0
+            int 21h
+
+            CheckKey_MemType_Op2Menu:
+                CALL WaitKeyPress
+
+
+            Push ax
+            PUSH dx 
+                ; Clear buffer
+                mov ah,07
+                int 21h
+                ; Reset Cursor
+                mov ah,2
+                mov dx, Op2CursorLoc
+                int 10h
+            pop dx 
+            pop ax
+
+            ; Check if pressed is Up or down or Enter
+            cmp ah, UpArrowScanCode                          
+            jz CommUp_Op2Menu 
+            cmp ah, DownArrowScanCode
+            jz CommDown_Op2Menu
+            cmp ah, EnterScanCode
+            jz Selected_Op2Menu
+            jmp CheckKey_MemType_Op2Menu
+
+
+            CommUp_Op2Menu:
+                mov ah, 9
+                ; Check overflow
+                    cmp dx, offset Mem0         ; the start of combobox
+                    jnz NotOverflow_Op2Menu
+                    mov dx, offset Mem15 ; last one to overcome overflow
+                    add dx, CommStringSize
+                NotOverflow_Op2Menu:
+                    sub dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_MemType_Op2Menu
+            
+            CommDown_Op2Menu:
+                mov ah, 9
+                ; Check End of file
+                    cmp dx, offset Mem15
+                    jnz NotEOF_Op2Menu
+                    mov dx, offset Mem0
+                    sub dx, CommStringSize
+                NotEOF_Op2Menu:
+                    add dx, CommStringSize
+                    int 21h
+                    jmp CheckKey_MemType_Op2Menu
+            
+            Selected_Op2Menu:
+                ; Detecting index of selected command
+                mov ax, dx
+                SUB AX, offset Mem0
+                mov bl, CommStringSize
+                div bl                                      ; Op=byte: AL:=AX / Op 
+                mov selectedOp2Mem, al
+
+                JMP RETURN_Op2Menu
+
+        EnterVal_Op2Menu:
+
+            ; Clear the space which the user should enter the value into
+            mov dx, offset ClearSpace
+            CALL DisplayString
+
+            ; Reset Cursor
+            mov dx, Op2CursorLoc
+            CALL SetCursor
+
+            ; Take value as a String from User
+            mov ah,0Ah                   
+            mov dx,offset num2
+            int 21h    
+            
+
+            mov cl,num2+1                 ;save string size
+            mov StrSize2,cl
+
+            mov ch,0
+            mov si,2                     ;si to get first number from string that is not zero
+            mov di,2  
+            
+            hoop2_Op2Menu:
+                mov dl,num2[di]               ; this loop to check zero in the first string
+                sub dl,30h
+                cmp dl,0 
+                jne hoop1_Op2Menu
+                inc si
+                mov cl, StrSize2
+                dec cl
+                mov StrSize2,cl 
+                inc di
+            jmp hoop2_Op2Menu
+            
+            hoop1_Op2Menu:
+                cmp cl,4         ;check that value is hexa or Get error    
+                jng sk_Op2Menu 
+                JMP InValidVal_Op2Menu
+
+            sk_Op2Menu:
+            
+            mov Bl,StrSize2              ; loops to check num from 1 to 9 and A to F
+            numloop_Op2Menu:
+                mov al,num2[si] 
+                cmp al,30H
+                jnge notnum_Op2Menu 
+                cmp al,39h
+                jnle notnum_Op2Menu
+                sub al,30h  
+                jmp done_Op2Menu   
+            
+            notnum_Op2Menu: 
+                cmp al,41H
+                jnge notcharnum_Op2Menu
+                cmp al,46h
+                jnle notcharnum_Op2Menu
+                sub al,37h
+                jmp done_Op2Menu  
+             
+            notcharnum_Op2Menu:
+                cmp al,61H
+                jnge InValidVal_Op2Menu
+                cmp al,66h
+                jnle InValidVal_Op2Menu
+                sub al,57h
+                jmp done_Op2Menu
+                
+                    
+            
+            done_Op2Menu:  
+                inc si       
+                cmp bl,4                   ; first digit
+                jne ha1_Op2Menu
+                mov cl,al 
+                mov ax,a 
+                mov ch,0
+                mul cx
+            ha1_Op2Menu:  
+                cmp bl,3                   ;second digit
+                jne ha2_Op2Menu
+                mov cl,al 
+                mov ax,b
+                mov ch,0
+                mul cx
+            
+            ha2_Op2Menu:  
+                cmp bl,2                   ;third digit
+                jne ha3_Op2Menu
+                mov cl,al 
+                mov ax,c
+                mov ch,0
+                mul cx
+            
+            ha3_Op2Menu:
+                cmp bl,1                  ;fourth digit
+                jne ha4_Op2Menu
+                mov ah,0
+            
+            ha4_Op2Menu:
+                add Op2Val,ax  
+            
+            dec Bl                    ; check to exit 
+            cmp Bl,0   
+            je RETURN_Op2Menu 
+            
+            jmp numloop_Op2Menu
+
+            InValidVal_Op2Menu:
+                MOV Op2Valid, 0
+                JMP RETURN_Op2Menu   
+
+        InvalidOp2Type:
+            ; TODO
+
+        RETURN_Op2Menu:
+
+            RET
+    Op2Menu ENDP
     END CommMenu
